42ece3e75baf0341516912aacb2ac8f8c250b18f
Athanasios <39865462+athanato@users.noreply.github.com>
2020-04-17 22:01:57 +0300

    "VehicleController.java" changes

--- src/main/java/com/project/vehicleid/Controller/VehicleController.java
+++ src/main/java/com/project/vehicleid/Controller/VehicleController.java
@@ -1,5 +1,7 @@
 package com.project.vehicleid.Controller;
 
+import com.project.vehicleid.Model.Vehicle;
+import com.project.vehicleid.Repository.VehicleRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("/")
 public class VehicleController {
     @GetMapping("/vehicle")
-    public list <vehicle> getAllVehicles() {
-        return userRepository.findAll();
+    public list <Vehicle> getAllVehicles() {
+        return VehicleRepository.findAll();
     }
 }
